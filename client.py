30db03b955f8a55e3907516638d2c725cc5b0c28
jayeshlohani <53624503+jayeshlohani@users.noreply.github.com>
2019-11-05 02:30:27 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,14 @@
 import socket
-import os
+from subprocess import PIPE, Popen
+
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
+
 s=socket.socket()
 #id="client1"
 while 1:
@@ -10,11 +19,24 @@ while 1:
     s.close()
     s=socket.socket()
     s.settimeout(1)
-    if os.system('netstat -aon | find /i "listening" | findstr "3001" |  find /c /v ""') >=1 :
-        s.connect(('127.0.0.1',p))
-        break 
-    break       
-    print('hello')     
+    if p==3001:
+        if int(cmdline('netstat -aon | find /i "listening" | findstr "3001" |  find /c /v ""').decode())==1:
+            s.connect(('127.0.0.1',p))
+            print(s.recv(1024).decode())
+            break
 
-print(s.recv(1024).decode())
+        else:
+            print('no port is available : service not available '+str(p))       
+            break
+    
+    elif p==3002:
+        if int(cmdline('netstat -aon | find /i "listening" | findstr "3002" |  find /c /v ""').decode())==1:
+            s.connect(('127.0.0.1',p))
+            print(s.recv(1024).decode())
+            break
+
+        else:
+            print('no port is available : service not available '+str(p))       
+            break
+
 s.close()
